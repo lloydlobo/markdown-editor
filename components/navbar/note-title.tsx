78bc12ed7ef56623d2abf5109c2df55d1245fefd
Lloyd Lobo <lloydlobo4@gmail.com>
2023-03-29 22:08:03 +0530

    style: style note name in navbar

--- components/navbar/note-title.tsx
+++ components/navbar/note-title.tsx
@@ -1,5 +1,44 @@
-import React from "react";
+import { ActionType, AppContext } from "@/store/AppContext";
+import {
+  Box,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+import React, { useContext } from "react";
 
 export default function NoteTitle() {
-  return <div>note-title</div>;
+  const { state, dispatch } = useContext(AppContext);
+  const { activeNote } = state;
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!activeNote) {
+      return;
+    }
+    dispatch({
+      type: ActionType.UPDATE_NOTE_NAME,
+      note: { ...activeNote, title: e.target.value },
+    });
+  };
+
+  return (
+    <Stack>
+      <Input
+        as={Input}
+        data-testid="titleInput"
+        onChange={(e) => handleOnChange(e)}
+        pointerEvents={activeNote ? "auto" : "none"}
+        disabled={activeNote ? false : true}
+        placeholder={activeNote ? "Untitled" : ""}
+        value={activeNote ? activeNote.title : ""}
+        _placeholder={{ opacity: 1, color: "whiteAlpha.700" }}
+        variant="flushed"
+        rounded="md"
+        borderColor={"transparent"}
+        px="2"
+      />
+    </Stack>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,27 +21,6 @@ export default function Home() {
       <Grid className="wrapper" autoFlow="row" maxH="100vh" h="full">
         <Navbar />
 
-        <Flex justify="space-between" align="center">
-          <Flex align="center" gap="2">
-            <Box>
-              <Button onClick={() => setIsOpen(!isOpen)}>Menu</Button>
-            </Box>
-            <chakra.div
-              textStyle="h4"
-              fontWeight="bold"
-              letterSpacing="wider"
-              textTransform="uppercase"
-            >
-              markdown
-            </chakra.div>
-          </Flex>
-
-          <Flex gap={[2, 4]}>
-            <Button rounded="md">Delete</Button>
-            <Button rounded="md">Save changes</Button>
-          </Flex>
-        </Flex>
-
         <Flex gap={[1, 2]} align="center">
           {isOpen ? (
             <Flex

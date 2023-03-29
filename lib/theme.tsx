e6bfad3ddb2e1d8ad7da27f4bafa3a8d41582a23
Lloyd Lobo <lloydlobo4@gmail.com>
2023-03-29 14:02:26 +0530

    feat: setup chakra ui

---
+++ lib/theme.tsx
@@ -0,0 +1,90 @@
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from '@chakra-ui/theme-tools'
+
+
+// Defualt styles.
+const styles = {
+  global: (props) => ({
+    body: {
+      fontFamily: 'body',
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+      bg: mode('white', 'gray.800')(props),
+      lineHeight: 'base',
+    },
+    '*::placeholder': {
+      color: mode('gray.400', 'whiteAlpha.400')(props),
+    },
+    '*, *::before, &::after': {
+      borderColor: mode('gray.200', 'whiteAlpha.300')(props),
+      wordWrap: 'break-word',
+    },
+  }),
+}
+
+/** 
+ * # Example
+ *
+ * `<Box textStyle='h1'>This is a box</Box>` 
+ */
+const textStyles = {
+  h1: {
+    // you can also use responsive styles
+    fontSize: ['48px', '72px'],
+    fontWeight: 'bold',
+    lineHeight: '110%',
+    letterSpacing: '-2%',
+  },
+  h2: {
+    fontSize: ['36px', '48px'],
+    fontWeight: 'semibold',
+    lineHeight: '110%',
+    letterSpacing: '-1%',
+  },
+  h3: {
+    fontSize: ['24px', '36px'],
+    fontWeight: 'semibold',
+    lineHeight: '110%',
+    letterSpacing: '-1%',
+  },
+  h4: {
+    fontSize: ['16px', '24px'],
+    fontWeight: 'semibold',
+    lineHeight: '110%',
+    letterSpacing: '-1%',
+  },
+};
+
+// These are the default breakpoints
+const breakpoints = {
+  sm: '30em', // 480px
+  md: '48em', // 768px
+  lg: '62em', // 992px
+  xl: '80em', // 1280px
+  '2xl': '96em', // 1536px
+}
+
+// Internally, we transform to this
+// const breakpoints = ['0em', '30em', '48em', '62em', '80em', '96em']
+
+export const theme = extendTheme({
+  styles,
+  textStyles,
+  breakpoints,
+});
+
+// // 1. Wrap the rendered mdx in a container
+// const MDXWrapper = (props) => <div className='mdx-prose' {...props} />
+// // 2. Define global styles
+// // PRO TIP: Again, you can also use the function version here
+// const theme = {
+//   styles: {
+//     global: {
+//       '.mdx-prose': {
+//         h1: { fontSize: 'xl', mb: '4', },
+//         p: { fontSize: 'sm', lineHeight: '1.4', },
+//       },
+//     },
+//   },
+// }
+//
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Roboto, Roboto_Slab, Roboto_Mono } from "next/font/google";
+import { theme } from "@/lib/theme";
 
+// <style jsx global>{`
+//   html { font-family: ${roboto.style.fontFamily} !important; }
+// `}</style>
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ChakraProvider theme={updateTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
+
+//
+// Google fonts with next need to be loaded in a tsx file here.
+// .
+
+const roboto = Roboto({ weight: ["300", "400"], subsets: ["latin"] });
+const robotoMono = Roboto_Mono({ subsets: ["latin"] });
+const robotoSlab = Roboto_Slab({
+  weight: ["300", "500", "700"],
+  subsets: ["latin"],
+});
+
+const fonts = {
+  heading: robotoSlab.style.fontFamily,
+  body: roboto.style.fontFamily,
+  code: robotoMono.style.fontFamily,
+};
+const updateTheme = extendTheme({ ...theme, fonts: fonts });

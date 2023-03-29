40b94ed45cb8ba7afb7eed7f313a522118ffc39a
Lloyd Lobo <lloydlobo4@gmail.com>
2023-03-29 14:15:10 +0530

    style: sidebar shifts editor and preview to side
    - hide the preview with from md: 1fr to base: 0

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,38 +6,45 @@ export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-    <Box h="full">
+      <Box h="full">
 
-      <Flex justify="space-between" align="center">
-        <Flex align="center" gap="2">
-          <Box><Button onClick={() => setIsOpen(!isOpen)}>Menu</Button></Box>
-          <chakra.div textStyle="h4" fontWeight="bold" letterSpacing="wider" textTransform="uppercase">markdown</chakra.div>
+        <Flex justify="space-between" align="center">
+          <Flex align="center" gap="2">
+            <Box><Button onClick={() => setIsOpen(!isOpen)}>Menu</Button></Box>
+            <chakra.div textStyle="h4" fontWeight="bold" letterSpacing="wider" textTransform="uppercase">markdown</chakra.div>
+          </Flex>
+
+          <Flex gap={[2, 4]}>
+            <Button rounded='md'>Delete</Button>
+            <Button rounded='md'>Save changes</Button>
+          </Flex>
         </Flex>
 
-        <Flex gap={[2, 4]}>
-          <Button rounded='md'>Delete</Button>
-          <Button rounded='md'>Save changes</Button>
-        </Flex>
-      </Flex>
+        <Center w="full">
+          <Grid w="full" gap={[2, 4]} gridTemplateColumns={{ 
 
-      <Center w="full">
-        <Grid w="full" gap={[2, 4]} gridTemplateColumns={{ md: isOpen ? 'min(33vw, 300px) 1fr 1fr' : '1fr 1fr' }}>
-          {isOpen ? (
-            <Flex position={{ base: "absolute", md: "relative" }} bg="whiteAlpha.500" _dark={{ bg: "blackAlpha.500" }} w="full" h="full">
-              <Stack  >
-                <StackItem>Document 1</StackItem>
-              </Stack>
-            </Flex>
-          ) : null}
+          base: isOpen ? 'min(33vw, 300px) 1fr 0' : '1fr 1fr',
+          md: isOpen ? 'min(33vw, 300px) 1fr 1fr' : '1fr 1fr',
 
-          <Textarea name="" id="" value={content} boxSize="fit-content" w="full" minHeight="container.sm" h="full" />
+          }}>
+            {isOpen ? (
+              <Flex
+                // position={{ base: "absolute", md: "relative" }}
+                bg="whiteAlpha.500" _dark={{ bg: "blackAlpha.500" }} w="full" h="full">
+                <Stack  >
+                  <StackItem>Document 1</StackItem>
+                </Stack>
+              </Flex>
+            ) : null}
 
-          <Box>
-            {content}
-          </Box>
-        </Grid>
-      </Center>
-    </Box>
+            <Textarea name="" id="" value={content} boxSize="fit-content" w="full" minHeight="container.sm" h="full" />
+
+            <Box>
+              {content}
+            </Box>
+          </Grid>
+        </Center>
+      </Box>
     </>
 
   );

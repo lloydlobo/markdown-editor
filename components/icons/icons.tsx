9cdf2ff1269431715acf8e58681345886f75f79c
Lloyd Lobo <lloydlobo4@gmail.com>
2023-03-29 23:51:54 +0530

    feat: change copy icon to document icon

--- components/icons/icons.tsx
+++ components/icons/icons.tsx
@@ -8,6 +8,7 @@ export function DocumentIcon(props: IconProps) {
       {...props}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
+      width="17" height="17"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
     >
--- components/navbar/note-title.tsx
+++ components/navbar/note-title.tsx
@@ -1,26 +1,24 @@
 import { ActionType, AppContext } from "@/store/AppContext";
-import { CopyIcon } from "@chakra-ui/icons";
 import {
   Box,
-  Button,
   chakra,
-  Editable,
-  EditableInput,
-  EditablePreview,
   Flex,
+  Hide,
   IconButton,
   Input,
-  Stack,
+  Tooltip,
   useToast,
 } from "@chakra-ui/react";
 import React, { useContext } from "react";
+import { DocumentIcon } from "../icons/icons";
 
 export default function NoteTitle() {
   const { state, dispatch } = useContext(AppContext);
   const { activeNote } = state;
+
   const toast = useToast();
 
-  const handleOnChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+  function handleOnChangeTitle(e: React.ChangeEvent<HTMLInputElement>) {
     if (!activeNote) {
       return;
     }
@@ -28,22 +26,46 @@ export default function NoteTitle() {
       type: ActionType.UPDATE_NOTE_NAME,
       note: { ...activeNote, title: e.target.value },
     });
-  };
+  }
 
-  const handleOnClickCopyMarkdown = () => {
-    let contentToCopy = activeNote?.content;
-    if (typeof contentToCopy !== 'undefined' && contentToCopy.length > 0) {
+  function handleOnClickCopyMarkdown() {
+    let contentToCopy = activeNote?.content.trim();
+    if (typeof contentToCopy !== "undefined" && contentToCopy.length > 0) {
       try {
         navigator.clipboard.writeText(contentToCopy);
-        toast({ title: "Copied markdown content", status: "success", duration: 3000, isClosable: true, });
+        toast({
+          title: "Copied markdown content",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
       } catch (err) {
-        toast({ title: "No content to copy", description: JSON.stringify(err), status: "error", duration: 3000, isClosable: true, });
+        toast({
+          title: "No content to copy",
+          description: JSON.stringify(err),
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
       }
     } else {
-      toast({ title: "No content to copy", status: "error", duration: 3000, isClosable: true, });
+      toast({
+        title: "No content to copy",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
     }
   }
 
+  // <>
+  //   <Show above='sm'>
+  //     <Box>This text appears at the "sm" value screen width or greater.</Box>
+  //   </Show>
+  //   <Hide below='md'>
+  //     <Box>This text hides at the "md" value screen width and smaller.</Box>
+  //   </Hide>
+  // </>
   return (
     <Flex
       overflowX="auto"
@@ -51,32 +73,38 @@ export default function NoteTitle() {
       opacity={activeNote ? "1" : "0"}
       pointerEvents={activeNote ? "auto" : "none"}
       alignItems="center"
-      title={activeNote ? `${activeNote.title.trim()}.md` : "untitled.md"}
     >
-      <IconButton
-        variant="ghost"
-        icon={<CopyIcon aria-hidden="true" />}
-        aria-label="copy markdown"
-        // size="sm"
-        onClick={handleOnClickCopyMarkdown}
-      />
-      <Box ml="2">
-        <chakra.label
-          lineHeight="none"
-          display={{ base: "none", md: "inline-block" }}
-          fontSize={{ base: "2xs", md: "xs" }}
-          _dark={{ color: "whiteAlpha.500" }}
-          color="blackAlpha.500"
-          textTransform="capitalize"
-        >
-          document name
-        </chakra.label>
+      <Tooltip label="copy markdown">
+        <IconButton
+          variant="ghost"
+          stroke="gray.300"
+          icon={<DocumentIcon aria-hidden="true" />}
+          aria-label="copy markdown"
+          onClick={handleOnClickCopyMarkdown}
+        />
+      </Tooltip>
+
+      <Box
+        ml="2"
+        title={activeNote ? `${activeNote.title.trim()}.md` : "untitled.md"}
+      >
+        <Hide below="md">
+          <chakra.label
+            lineHeight="none"
+            fontSize={{ base: "2xs", md: "xs" }}
+            _dark={{ color: "whiteAlpha.500" }}
+            color="blackAlpha.500"
+            textTransform="capitalize"
+          >
+            document name
+          </chakra.label>
+        </Hide>
         <Input
           my="0"
           data-testid="titleInput"
           value={activeNote ? activeNote.title : ""}
           onChange={(e) => handleOnChangeTitle(e)}
-        size="sm"
+          size="sm"
           pointerEvents={activeNote ? "auto" : "none"}
           disabled={activeNote ? false : true}
           placeholder={activeNote ? "Untitled" : ""}

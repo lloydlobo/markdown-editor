56a4ace671a1b61133bc1c68ec7fa0c19a619196
Lloyd Lobo <lloydlobo4@gmail.com>
2023-03-31 20:17:10 +0530

    style: restrict max width of preview in full screen

--- components/preview/preview.tsx
+++ components/preview/preview.tsx
@@ -13,6 +13,8 @@ export default function Preview() {
         overflowY={"scroll"}
         h={"calc(100vh - 80px)"}
         minH={"calc(100vh - 80px)"}
+        maxW={{ md: "61.8vw" }}
+        marginInline="auto"
       >
         <PreviewBody />
       </Box>

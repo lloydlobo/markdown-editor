b65a84824a147aea8733da29c66e739423b82165
Lloyd Lobo <lloydlobo4@gmail.com>
2023-04-01 14:15:04 +0530

    fix: nest children between html tags not props
    Why:
    
    - syntax highlighter was accepting preview body
      text as props
    
    Fixes:
    
    - 76:21  Error: Do not pass children as props.
      Instead, nest children between the opening
      and closing tags.
    - 90:19  Error: Do not pass children as props.
      Instead, nest children between the opening
      and closing tags.  react/no-children-prop

--- components/preview/preview-body.tsx
+++ components/preview/preview-body.tsx
@@ -1,66 +1,62 @@
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import { ActionType, AppContext } from "@/store/AppContext";
-import React, { useContext, useState } from "react";
-import { useColorMode, useColorModeValue } from "@chakra-ui/system";
-
-
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import oneDark from "react-syntax-highlighter/dist/cjs/styles/prism/one-dark";
-import oneLight from "react-syntax-highlighter/dist/cjs/styles/prism/one-light";
-
-import {
-  Box,
-  Button,
-  Stack,
-  Center,
-  AbsoluteCenter,
-  Table,
-} from "@chakra-ui/react";
-import { PlusSquareIcon } from "@chakra-ui/icons";
-import { INote } from "@/types/inote";
-import { nanoid } from "nanoid";
-import { getTimestamp } from "@/utils/get-timestamp";
-import { CustomLink } from "@/components/common/custom-link";
-import remarkGfm from "remark-gfm";
-import { mode } from "@chakra-ui/theme-tools";
+/**
+ * This component renders the preview of a Markdown body.
+ * If there is no active note, it renders a "Create new" button instead.
+ */
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import { ActionType, AppContext } from '@/store/AppContext';
+import { useColorModeValue } from '@chakra-ui/system';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import remarkGfm from 'remark-gfm';
+import oneDark from 'react-syntax-highlighter/dist/cjs/styles/prism/one-dark';
+import oneLight from 'react-syntax-highlighter/dist/cjs/styles/prism/one-light';
+import { Box, Button, Center, Stack } from '@chakra-ui/react';
+import { PlusSquareIcon } from '@chakra-ui/icons';
+import { CustomLink } from '@/components/common/custom-link';
+import { getTimestamp } from '@/utils/get-timestamp';
+import { INote } from '@/types/inote';
+import { nanoid } from 'nanoid';
+import React, { useContext } from 'react';
 
 export default function PreviewBody() {
   const { state, dispatch } = useContext(AppContext);
   const { activeNote, isPreview } = state;
 
   const syntaxStyleTheme = useColorModeValue(oneLight, oneDark);
-
+  /**
+   * This component renders the preview of a Markdown body.
+   * If there is no active note, it renders a "Create new" button instead.
+   */
   const addNewNote = () => {
     const newNote: INote = {
-      id: state.notes ? state.notes?.length + 1 : -1,
+      id: state.notes ? state.notes.length + 1 : -1,
       nanoid: nanoid(),
-      title: "Untitled",
-      content: "",
+      title: 'Untitled',
+      content: '',
       createdAt: getTimestamp(),
     };
     dispatch({ type: ActionType.ADD_NOTE, note: newNote });
   };
-
+  /**
+   * This component renders the preview of a Markdown body.
+   * If there is no active note, it renders a "Create new" button instead.
+   */
   if (!activeNote) {
     return (
       <Center pos="relative" h="full">
-        <AbsoluteCenter>
-          <Box>
-            <Button leftIcon={<PlusSquareIcon />} onClick={addNewNote}>
-              Create new
-            </Button>
-          </Box>
-        </AbsoluteCenter>
+        <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)">
+          <Button leftIcon={<PlusSquareIcon />} onClick={addNewNote}>
+            Create new
+          </Button>
+        </Box>
       </Center>
     );
   }
-
   return (
     <Box
       className="preview-markdown"
       w="full"
-      maxWidth={{ md: isPreview ? "75vw" : "50vw" }}
-      marginInline={{ md: isPreview ? "auto" : "auto" }}
+      maxWidth={{ md: isPreview ? '75vw' : '50vw' }}
+      marginInline={{ md: 'auto' }}
       p="6"
     >
       <Stack pb="12">
@@ -69,28 +65,16 @@ export default function PreviewBody() {
           components={{
             a: CustomLink,
             code({ node, inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || "");
-              if (typeof props !== undefined) {
-                return !inline && match ? (
-                  <SyntaxHighlighter
-                    children={String(children).replace(/\n$/, "")}
-                    {...props}
-                    style={syntaxStyleTheme}
-                    language={match[1]}
-                    PreTag="div"
-                  />
-                ) : (
-                  <code className={className} {...props}>
-                    {children}
-                  </code>
-                );
-              }
+              const match = /language-(\w+)/.exec(className || '');
               return !inline && match ? (
                 <SyntaxHighlighter
-                  children={String(children).replace(/\n$/, "")}
+                  {...props}
+                  style={syntaxStyleTheme}
                   language={match[1]}
                   PreTag="div"
-                />
+                >
+                  {String(children).replace(/\n$/, '')}
+                </SyntaxHighlighter>
               ) : (
                 <code className={className} {...props}>
                   {children}
@@ -98,11 +82,11 @@ export default function PreviewBody() {
               );
             },
           }}
-
         >
-          {activeNote?.content ? activeNote.content : ""}
+          {activeNote?.content || ''}
         </ReactMarkdown>
       </Stack>
     </Box>
   );
 }
+

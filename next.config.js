772c51cab57eb2d99fdf49f794d2c8dfaa37af5d
Lloyd Lobo <lloydlobo4@gmail.com>
2023-04-01 17:48:02 +0530

    chore: add dotenv dependency wip

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   // Toggle to false to see if component doesn't render twice.
   reactStrictMode: true,
+  // env: {
+  //   NODE_ENV: "development",
+  // },
 }
 
 module.exports = nextConfig

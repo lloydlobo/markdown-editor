3879ac1c3d8a885dcf334292b5a3360aeeb9626a
Lloyd Lobo <lloydlobo4@gmail.com>
2023-04-02 12:11:57 +0530

    feat: route logo on click to index

--- components/sidebar/sidebar-logo.tsx
+++ components/sidebar/sidebar-logo.tsx
@@ -1,17 +1,28 @@
-import { Box, Text, chakra } from '@chakra-ui/react'
-import React from 'react'
+import { Link as ChakraLink, Box, Text } from "@chakra-ui/react";
+import React from "react";
 
 export default function Logo() {
   return (
-    <Box
-      // display={{ sm: "none", md: "flex" }}
-      alignItems="center"
-      pr="4"
-      borderInlineEnd="thin"
-      borderInlineEndColor="blackAlpha.400"
-      h="full"
-    >
-      <Text my="0" py="0" fontSize={{ base: "sm", md: "sm" }} lineHeight="base" fontWeight="bold" letterSpacing="0.5ch" textTransform="uppercase">markdown</Text>
-    </Box>
-  )
+    <ChakraLink as={ChakraLink} href="/">
+      <Box
+        borderInlineEndColor="blackAlpha.400"
+        borderInlineEnd="thin"
+        alignItems="center"
+        h="full"
+        pr="4"
+      >
+        <Text
+          my="0"
+          py="0"
+          fontSize={{ base: "sm", md: "sm" }}
+          lineHeight="base"
+          fontWeight="bold"
+          letterSpacing="0.5ch"
+          textTransform="uppercase"
+        >
+          markdown
+        </Text>
+      </Box>
+    </ChakraLink>
+  );
 }
